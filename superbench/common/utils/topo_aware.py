9dffe9f6ae4a1324a035fc6a2236106b2c3bd7f3
Yang Wang <yangwang1@microsoft.com>
2022-08-18 10:37:15 +0800

    add

--- superbench/common/utils/topo_aware.py
+++ superbench/common/utils/topo_aware.py
@@ -41,32 +41,32 @@ def gen_ibstat_file(ibstat_file):
     Args:
         ibstat_file (str): path of ibstat output.
     """
-    import mpi4py
-    mpi4py.rc(initialize=False, finalize=False)
-    from mpi4py import MPI
+    # import mpi4py
+    # mpi4py.rc(initialize=False, finalize=False)
+    # from mpi4py import MPI
 
-    # if not MPI.Is_initialized():
-    #     MPI.Init()
+    # # if not MPI.Is_initialized():
+    # #     MPI.Init()
 
-    comm = MPI.COMM_WORLD
-    name = MPI.Get_processor_name()
+    # comm = MPI.COMM_WORLD
+    # name = MPI.Get_processor_name()
 
-    # The command to fetch ibstat info
-    cmd = r"ibstat | grep -Po 'System image GUID: \K\S+$'"
-    output = os.popen(cmd)
-    ibstat = 'VM_hostname ' + name + '\n' + str(output.read())
+    # # The command to fetch ibstat info
+    # cmd = r"ibstat | grep -Po 'System image GUID: \K\S+$'"
+    # output = os.popen(cmd)
+    # ibstat = 'VM_hostname ' + name + '\n' + str(output.read())
 
-    # Fetch all ibstate from each node
-    ibstats = comm.allgather(ibstat)
+    # # Fetch all ibstate from each node
+    # ibstats = comm.allgather(ibstat)
 
-    ibstate_file_path = Path(ibstat_file)
+    # ibstate_file_path = Path(ibstat_file)
 
-    # Filter the duplicate info
-    ibstat_infos = set(ibstats)
+    # # Filter the duplicate info
+    # ibstat_infos = set(ibstats)
 
-    with ibstate_file_path.open(mode='w') as f:
-        for ibstat_info in ibstat_infos:
-            f.write(ibstat_info)
+    # with ibstate_file_path.open(mode='w') as f:
+    #     for ibstat_info in ibstat_infos:
+    #         f.write(ibstat_info)
     # MPI.Finalize()
 
 
