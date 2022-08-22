60bdc604502db8afb8429f735443aa01f0b70ebc
Yang Wang <yangwang1@microsoft.com>
2022-08-22 19:32:28 +0800

    add

--- superbench/benchmarks/micro_benchmarks/ib_validation_performance/ib_validation_performance.cc
+++ superbench/benchmarks/micro_benchmarks/ib_validation_performance/ib_validation_performance.cc
@@ -416,14 +416,15 @@ void output_to_file(const std::string cmd_prefix, const vector<vector<std::pair<
 int main(int argc, char **argv) {
     try {
         // Initialize the MPI environment.
+        printf("start to init the MPI!\n");
         int flag;
         MPI_Initialized(&flag);
-        // printf("The result of MPI_Initialized is %d\n",flag);
-        // if(flag != 1){
-        //     int provided;
-        //     printf("MPI_Initialized returned false before MPI_Init.\n");
-        //     MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
-        // }
+        printf("The result of MPI_Initialized is %d\n",flag);
+        if(flag != 1){
+            int provided;
+            printf("MPI_Initialized returned false before MPI_Init.\n");
+            MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
+        }
         printf("The result of MPI_Init_thread is %d\n",flag);
         // Get the number of ranks
         MPI_Comm_size(MPI_COMM_WORLD, &g_world_size);
