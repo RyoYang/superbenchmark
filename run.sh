d6909ca307a81d825da72229acafb8cfb156adcf
Yang Wang <yangwang1@microsoft.com>
2023-01-03 18:46:08 +0800

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,5 @@ git add .
 git commit -m "update"
 git push origin test_ncll
 ansible-playbook -i remote.ini install.yaml
-sb run -c ib.yaml -f remote.ini
 python3 -m pip install -e .
 sb run -c ib.yaml -f remote.ini
